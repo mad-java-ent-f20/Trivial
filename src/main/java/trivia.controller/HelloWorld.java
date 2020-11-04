9f9e2f7c3721f92354f24a582fa51eed78158732
GuruKatuwal <gkatuwal@madisoncollege.edu>
2020-11-04 15:51:48 -0600

    Added user in services

--- src/main/java/trivia.controller/HelloWorld.java
+++ src/main/java/trivia.controller/HelloWorld.java
@@ -2,18 +2,21 @@ package trivia.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/users")
 public class HelloWorld {
-    // The Java method will process HTTP GET requests
+     // The Java method will process HTTP GET requests
     @GET
+
+    @Path("{name}")
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
-    public Response getMessage() {
-        // Return a simple message
-        String output = "Hello World";
+    public Response getUserByName(@PathParam("name") String name) {
+              // Return a simple message
+        String output =  name + ",  Welcome to our Project";
         return Response.status(200).entity(output).build();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h1>Hello World!</h1>
+<h2>Project name: Triviayou</h2>
+<h2>Team name: Trivial Trivia</h2>
+<h2>Members: Elizabeth, Guru, Angel</h2>
+
+<a href="http://localhost:8080/Trivia_war/services/users/">Click Here</a>
 </body>
 </html>
